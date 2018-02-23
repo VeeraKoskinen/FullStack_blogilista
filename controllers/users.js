c5a88590895a324c98b2f2ccc26c93500b854457
Veera Koskinen <veera.koskinen@ren.to>
2018-02-23 17:16:13 +0200

    Tehtävä 4.16

--- controllers/users.js
+++ controllers/users.js
@@ -5,35 +5,47 @@ const User = require('../models/user')
 usersRouter.get('/', async (request, response) => {
     const users = await User.find({})
     response.json(users.map(User.format))
-  })
-  
+})
+
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const body = request.body
+    try {
+        const body = request.body
 
-    const existingUser = await User.find({username: body.username})
-      if (existingUser.length>0) {
-        return response.status(400).json({ error: 'username must be unique' })
-      }
+        const existingUser = await User.find({ username: body.username })
+        if (existingUser.length > 0) {
+            return response.status(400).json({ error: 'username must be unique' })
+        }
 
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+        if (body.password.length < 3 ) {
+            return response.status(400).json({error: 'password must be at least 3 letters'})
+        }
 
-    const user = new User({
-      username: body.username,
-      name: body.name,
-      passwordHash,
-      adult: body.adult
-    })
+        let grownup = true
 
-    const savedUser = await user.save()
+        if (body.adult === undefined) {
+            grownup = true
+        } else {
+            grownup = body.adult
+        }
 
-    response.json(User.format(savedUser))
-  } catch (exception) {
-    console.log(exception)
-    response.status(500).json({ error: 'something went wrong...' })
-  }
+        const saltRounds = 10
+        const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+        const user = new User({
+            username: body.username,
+            name: body.name,
+            passwordHash,
+            adult: grownup
+        })
+
+        const savedUser = await user.save()
+
+        response.json(User.format(savedUser))
+    } catch (exception) {
+        console.log(exception)
+        response.status(500).json({ error: 'something went wrong...' })
+    }
 })
 
 module.exports = usersRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -170,7 +170,7 @@ describe('update blog properly', async () => {
     test('PUT /api/blogs:id manage to update the title', async () => {
         const blogsAtStart = await blogsInDb()
 
-        const updatedBlog = new Blog ({
+        const updatedBlog = new Blog({
             title: "Uutuudesta nouseva",
             author: "Kirkuva Kurppa",
             url: "www.vanhuksetBloggaa.fi",
@@ -192,7 +192,7 @@ describe('update blog properly', async () => {
     test('PUT /api/blogs:id manage to update the author', async () => {
         const blogsAtStart = await blogsInDb()
 
-        const updatedBlog = new Blog ({
+        const updatedBlog = new Blog({
             title: "Uutuudesta nouseva",
             author: "Kirkuva Karppi",
             url: "www.vanhuksetBloggaa.fi",
@@ -214,56 +214,78 @@ describe('update blog properly', async () => {
 
 describe.only('when there is initially one user at db', async () => {
     beforeAll(async () => {
-      await User.remove({})
-      const user = new User({ username: 'root', password: 'sekret', adult: true })
-      await user.save()
+        await User.remove({})
+        const user = new User({ username: 'root', password: 'sekret', adult: true })
+        await user.save()
     })
-  
-    test('POST /api/users succeeds with a fresh username', async () => {
-      const usersBeforeOperation = await usersInDb()
-  
-      const newUser = {
-        username: 'mluukkai',
-        name: 'Matti Luukkainen',
-        password: 'salainen',
-        adult: true
-      }
-  
-      await api
-        .post('/api/users')
-        .send(newUser)
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-  
-      const usersAfterOperation = await usersInDb()
-      expect(usersAfterOperation.length).toBe(usersBeforeOperation.length+1)
-      const usernames = usersAfterOperation.map(u=>u.username)
-      expect(usernames).toContain(newUser.username)
+
+    test('POST /api/users succeeds with a fresh username which do not have an adult-value', async () => {
+        const usersBeforeOperation = await usersInDb()
+
+        const newUser = {
+            username: 'mluukkai',
+            name: 'Matti Luukkainen',
+            password: 'salainen',
+            adult: true
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAfterOperation = await usersInDb()
+        expect(usersAfterOperation.length).toBe(usersBeforeOperation.length + 1)
+        const usernames = usersAfterOperation.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
     })
 
     test('POST /api/users fails with proper statuscode and message if username already taken', async () => {
         const usersBeforeOperation = await usersInDb()
-      
+
         const newUser = {
-          username: 'root',
-          name: 'Superuser',
-          password: 'salainen'
+            username: 'root',
+            name: 'Superuser',
+            password: 'salainen'
         }
-      
+
         const result = await api
-          .post('/api/users')
-          .send(newUser)
-          .expect(400)
-          .expect('Content-Type', /application\/json/)
-      
-        expect(result.body).toEqual({ error: 'username must be unique'})
-      
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body).toEqual({ error: 'username must be unique' })
+
         const usersAfterOperation = await usersInDb()
         expect(usersAfterOperation.length).toBe(usersBeforeOperation.length)
-      })
+    })
+
+    test('POST /api/users fails with proper statuscode and message if password is too sort', async () => {
+        const usersBeforeOperation = await usersInDb()
+
+        const newUser = {
+            username: 'suppilo',
+            name: 'suppilo',
+            password: 'sa',
+            adult: false
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body).toEqual({ error: 'password must be at least 3 letters' })
+
+        const usersAfterOperation = await usersInDb()
+        expect(usersAfterOperation.length).toBe(usersBeforeOperation.length)
+    })
 
     afterAll(() => {
         server.close()
     })
-  })
+})
 
